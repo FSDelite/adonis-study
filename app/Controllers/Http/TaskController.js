d495936f2821686044b4093391c8f8e61675638b
FSDelite <felipedelite@gmail.com>
2021-10-06 17:09:59 -0400

    teste timediff

--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -40,7 +40,7 @@ class TaskController {
     const inicio = new Date(data.start_date).getTime(); // pegando o tempo em milissegundos
     const fim = new Date(data.finish_date).getTime(); // pegando o tempo em milissegundos
 
-    const final = ((fim - inicio) / 3600000) * hour_value; //Convertendo em horas
+    const final = Math.ceil(((fim - inicio) / 3600000) * hour_value); //Convertendo em horas
     
     const datanovo = { final_value: final, ...data };
 
