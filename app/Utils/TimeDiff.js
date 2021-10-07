285d4f26e784b2a4a922cc99ce00c3c0702faaa2
FSDelite <felipedelite@gmail.com>
2021-10-07 14:14:13 -0400

    change ceil to round in TimeDiff class

--- app/Utils/TimeDiff.js
+++ app/Utils/TimeDiff.js
@@ -3,7 +3,7 @@ class TimeDiff {
     const inicio = new Date(start_date).getTime(); // pegando o tempo em milissegundos
     const fim = new Date(finish_date).getTime(); // pegando o tempo em milissegundos
 
-    const final = Math.ceil((fim - inicio) / 3600000) * hour_value; //Convertendo em horas
+    const final = Math.round((fim - inicio) / 3600000) * hour_value; //Convertendo em horas
     return final;
   }
 }
